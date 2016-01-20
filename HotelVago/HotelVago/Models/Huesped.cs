d11d489bbe049a953377598e9badd12f3e326954
LeobardoMorales <leo_96@hotmail.es>
2016-01-20 09:38:27 -0700

    Cree el modelo huesped

--- HotelVago/HotelVago/Models/Huesped.cs
+++ HotelVago/HotelVago/Models/Huesped.cs
@@ -7,5 +7,13 @@ namespace HotelVago.Models
 {
     public class Huesped
     {
+        public int huespedID { get; set; }
+        public String nombre { get; set; }
+        public String apellidoP { get; set; }
+        public String apellidoM { get; set; }
+        public String telefono { get; set; }
+
+        // Huesped tiene una coleccion de reservaciones
+        public ICollection<Reservacion> reservaciones { get; set; }
     }
 }
