0aad3c6ba40b226957c4f9496660a80b252ad66d
jose morales <panfilo.morales101@hotmail.com>
2016-01-20 09:41:32 -0600

    Se creo el Modelo de Habitacion

--- HotelVago/HotelVago/Models/Habitacion.cs
+++ HotelVago/HotelVago/Models/Habitacion.cs
@@ -7,5 +7,12 @@ namespace HotelVago.Models
 {
     public class Habitacion
     {
+        public int habitacionID { get; set; }
+        public string tamañoHabitacion { get; set; }
+        public Boolean estado { get; set; }
+
+
+        //una Habitación ouede tener muchas Recervaciones
+        public ICollection<Reservacion> reservaciones;
     }
 }
